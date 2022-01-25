c5b0ad9ca0a11ee535a0593244987ab62e2484b2
Zhixian Ong <zhixian@hotmail.com>
2022-01-25 15:21:58 +0800

    init commit

---
+++ Mini.Common/Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Mini.Common.Requests;
+
+[ExcludeFromCodeCoverage]
+public class LoginRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ Mini.Common/Responses/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Mini.Common.Responses;
+
+[ExcludeFromCodeCoverage]
+internal class LoginResponse
+{
+    public string Jwt { get; set; } = string.Empty;
+
+    public DateTime ExpiryDateTime { get; set; }
+}
