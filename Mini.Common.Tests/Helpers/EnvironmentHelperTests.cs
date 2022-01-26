7ab789ba5bf942a783964c725177e0b2a1172e67
zhixian <zhixian@hotmail.com>
2022-01-26 19:23:36 +0800

    Add EnvironmentHelper

---
+++ Mini.Common.Tests/Helpers/EnvironmentHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mini.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini.Common.Helpers.Tests
+{
+    [TestClass()]
+    public class EnvironmentHelperTests
+    {
+        [TestMethod()]
+        public void GetVariableTest()
+        {
+            Environment.SetEnvironmentVariable("someVariable", "someValue");
+
+            string someValue = EnvironmentHelper.GetVariable("someVariable");
+
+            Assert.AreEqual<string>("someValue", someValue);
+        }
+
+        [TestMethod()]
+        public void GetVariableNullTest()
+        {
+            Environment.SetEnvironmentVariable("someVariable", null);
+
+            var ex = Assert.ThrowsException<InvalidOperationException>(() => 
+                EnvironmentHelper.GetVariable("someVariable"));
+
+            Assert.IsNotNull(ex);
+            Assert.AreEqual<string>("Environment variable someVariable does not exists.", ex.Message);
+        }
+    }
+}
---
+++ Mini.Common/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Mini.Common.Helpers;
+
+public static class EnvironmentHelper
+{
+    // We cannot add extension methods to a static class.
+
+    public static string GetVariable(string variableName)
+    {
+        string? variableValue = Environment.GetEnvironmentVariable(variableName);
+
+        if (variableValue == null)
+            throw new InvalidOperationException($"Environment variable {variableName} does not exists.");
+
+        return variableValue;
+    }
+}
