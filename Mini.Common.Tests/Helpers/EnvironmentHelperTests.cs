2f92c25e295109a4f49d65e79b384f8ffcbf7bab
zhixian <zhixian@hotmail.com>
2022-01-26 20:01:46 +0800

    Add EnvironmentHelperTests

--- Mini.Common.Tests/Helpers/EnvironmentHelperTests.cs
+++ Mini.Common.Tests/Helpers/EnvironmentHelperTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Mini.Common.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mini.Common.Helpers.Tests
 {
