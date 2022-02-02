fa7c8b10c3edfab7c6b2020d93ba6fd0591668f3
zhixian <zhixian@hotmail.com>
2022-02-02 23:34:49 +0800

    update

---
+++ Mini.Wms/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Mini.Wms;
+
+public interface ICustomer
+{
+    string Id { get; init; }
+
+    string Name { get; init; }
+
+    DateTime CreatedDateTime { get; init; }
+}
+
+public readonly record struct Customer(string Id, string Name, DateTime CreatedDateTime) : ICustomer
+{
+    public override string ToString() =>
+        $"Id:{Id}, Name:{Name}, CreatedDateTime:{CreatedDateTime:s}";
+
+}
---
+++ Mini.Wms/Services/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿namespace Mini.Wms.Services;
+
+public interface ICustomerService
+{
+    IList<ICustomer> GetCustomerList();
+
+    void Add(ICustomer customer);
+
+    void Update(ICustomer customer);
+
+    void Archive(ICustomer customer);
+
+    void Remove(ICustomer customer);
+
+}
+
+public class CustomerService : ICustomerService
+{
+    public void Add(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Archive(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<ICustomer> GetCustomerList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+}
