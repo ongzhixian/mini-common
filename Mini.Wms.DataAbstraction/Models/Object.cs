7f593d0259750960e121246368738d625f84c348
zhixian <zhixian@hotmail.com>
2022-02-06 18:35:00 +0800

    update

---
+++ Mini.Wms.DataAbstraction/Models/Object.cs
@@ -0,0 +1,22 @@
+﻿namespace Mini.Wms.DataAbstraction.Models;
+
+public interface IObject<T>
+{
+    T Id { get; init; }
+    DateTime CreatedDateTime { get; init; }
+    DateTime LastUpdatedDateTime { get; init; }
+}
+
+public interface IUser<T> : IObject<T>
+{
+    string Username { get; init; }
+    string FirstName { get; init; }
+    string LastName { get; init; }
+    string Password { get; init; }
+    DateTime PasswordUpdatedDateTime { get; init; }
+}
+
+public interface ICustomer<T> : IObject<T>
+{
+    string Name { get; init; }
+}
---
+++ Mini.Wms.DataAbstraction/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Mini.Wms.DataAbstraction.Models;
+
+namespace Mini.Wms.Abstraction.Services;
+
+public interface IUserService<T>
+{
+    void Add(IUser<T> user);
+
+    void Update(IUser<T> user);
+
+    void Delete(IUser<T> user);
+
+    void Get(T id);
+}
