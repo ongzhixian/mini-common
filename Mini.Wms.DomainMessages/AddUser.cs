3ff4d096303f4e0210b81712f6820f00543a3863
Zhixian Ong <zhixian@hotmail.com>
2022-02-07 13:27:44 +0800

    add wms-domainMessages

---
+++ Mini.Wms.DomainMessages/AddUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Mini.Wms.DomainMessages;
+public readonly record struct AddUser
+{
+    public string Username { get; init; } = string.Empty;
+
+    public string FirstName { get; init; } = string.Empty;
+
+    public string LastName { get; init; } = string.Empty;
+}
