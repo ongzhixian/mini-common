1a6b8e08ec091a1e086605625dabe9293f3e8e2f
Zhixian Ong <zhixian@hotmail.com>
2022-02-07 15:11:44 +0800

    NewUserMessage

---
+++ Mini.Wms.DomainMessages/NewUserMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Mini.Wms.DomainMessages;
+
+public interface INewUserMessage
+{
+    string Username { get; init; }
+    string FirstName { get; init; }
+    string LastName { get; init; }
+
+}
+
+public readonly record struct NewUserMessage(string Username, string FirstName, string LastName) : INewUserMessage;
