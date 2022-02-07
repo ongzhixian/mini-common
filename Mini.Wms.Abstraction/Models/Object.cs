28777657028dd25588e87cd6d72601220a11d27f
Zhixian Ong <zhixian@hotmail.com>
2022-02-07 11:21:10 +0800

    Differentiating dbObject and domain objects

--- Mini.Wms.Abstraction/Models/Object.cs
+++ Mini.Wms.Abstraction/Models/Object.cs
@@ -1,22 +1,32 @@
 ﻿namespace Mini.Wms.Abstraction.Models;
 
-public interface IObject<T>
+public interface IDbObject<T>
 {
     T Id { get; init; }
     DateTime CreatedDateTime { get; init; }
     DateTime LastUpdatedDateTime { get; init; }
 }
 
-public interface IUser<T> : IObject<T>
+public interface IUser<T> : IDbObject<T>, IUser
+{
+    DateTime PasswordUpdatedDateTime { get; init; }
+}
+
+public interface IUser
 {
     string Username { get; init; }
     string FirstName { get; init; }
     string LastName { get; init; }
     string Password { get; init; }
-    DateTime PasswordUpdatedDateTime { get; init; }
 }
 
-public interface ICustomer<T> : IObject<T>
+
+public interface ICustomer<T> : IDbObject<T>, ICustomer
+{
+    string Name { get; init; }
+}
+
+public interface ICustomer
 {
     string Name { get; init; }
 }
