31fd2ac591298ec2fe9e21eded94dfe9ae4da372
Zhixian Ong <zhixian@hotmail.com>
2022-02-08 13:32:08 +0800

    update unit tests

---
+++ Mini.Wms.MongoDbImplementation.Tests/Integration/Services/UserServiceTests.cs
@@ -0,0 +1,144 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mini.Wms.MongoDbImplementation.Models;
+using Mini.Wms.MongoDbImplementation.Services;
+using MongoDB.Driver;
+using Moq;
+
+namespace Mini.Wms.MongoDbImplementation.Tests.Integration.Services;
+
+[TestCategory("Integration")]
+[TestClass()]
+public class UserServiceTests
+{
+    private readonly IConfigurationRoot? config = new ConfigurationBuilder()
+        .AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    private Mock<ILogger<UserService>> mockLogger = new Mock<ILogger<UserService>>();
+
+    private IMongoClient mongoClient;
+    private IMongoDatabase database;
+    private IMongoCollection<User> userCollection;
+    private UserService? userService;
+
+    public UserServiceTests()
+    {
+        string miniToolsConnectionString = config["mongodb:minitools:ConnectionString"];
+        string databaseName = MongoUrl.Create(miniToolsConnectionString).DatabaseName;
+
+        mongoClient = new MongoClient(miniToolsConnectionString);
+        database = mongoClient.GetDatabase(databaseName);
+        userCollection = database.GetCollection<User>(typeof(User).Name);
+    }
+
+    //[ClassInitialize]
+    //public async Task BeforeAllTestAsync()
+    //{
+    //    //var options = new CreateIndexOptions() { Unique = true };
+    //    //var field = new StringFieldDefinition<User>("Username");
+    //    //var indexDefinition = new IndexKeysDefinitionBuilder<User>().Ascending(field);
+    //    //CreateIndexModel<User> createIndexModel = new CreateIndexModel<User>(indexDefinition, options);
+    //    //var x = await userCollection.Indexes.CreateOneAsync(createIndexModel);
+    //}
+
+    [TestInitialize]
+    public void BeforeEachTest()
+    {
+        mockLogger = new Mock<ILogger<UserService>>();
+
+        string miniToolsConnectionString = config["mongodb:minitools:ConnectionString"];
+
+        string databaseName = MongoUrl.Create(miniToolsConnectionString).DatabaseName;
+
+        mongoClient = new MongoClient(miniToolsConnectionString);
+        database = mongoClient.GetDatabase(databaseName);
+        userCollection = database.GetCollection<User>(typeof(User).Name);
+
+    }
+
+    [TestMethod()]
+    public void UserServiceTest()
+    {
+        userService = new UserService(mockLogger.Object, userCollection);
+
+        Assert.IsNotNull(userService);
+    }
+
+    [TestMethod()]
+    public async Task AddAsyncTestAsync()
+    {
+        userService = new UserService(mockLogger.Object, userCollection);
+
+        // 6201bfe6be1eb4cc604fd8be
+        // 000000000000000000000000
+        //var oid = new MongoDB.Bson.ObjectId("000000000000000000000000");
+
+        User record = new User()
+        {
+            
+            Id = "000000000000000000000000",
+            Username = "someUsername5",
+            FirstName = "someFirstName3",
+            LastName = "someLastName3"
+        };
+
+        await userService.AddAsync(record);
+
+        Assert.IsNotNull(record.Id);
+    }
+
+
+    [TestMethod()]
+    public async Task AllAsyncTestAsync()
+    {
+        userService = new UserService(mockLogger.Object, userCollection);
+
+        var result = await userService.AllAsync();
+
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod()]
+    public async Task DeleteAsyncTestAsync()
+    {
+
+        var result = await userService.DeleteAsync("000000000000000000000000");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod()]
+    public async Task GetAsyncTestAsync()
+    {
+        userService = new UserService(mockLogger.Object, userCollection);
+
+        var result = await userService.GetAsync("000000000000000000000000");
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("someUsername", result.Username);
+    }
+
+    [TestMethod()]
+    public async Task UpdateAsyncTestAsync()
+    {
+        userService = new UserService(mockLogger.Object, userCollection);
+
+        User record = new User()
+        {
+            Id = "000000000000000000000000",
+            Username = "someUsername3",
+            FirstName = "chgFirstName4",
+            LastName = "chgLastName4"
+        };
+
+        var result = await userService.UpdateAsync(record);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("someUsername", result.Username);
+    }
+
+}
--- Mini.Wms.MongoDbImplementation.Tests/Services/UserServiceTests.cs
+++ Mini.Wms.MongoDbImplementation.Tests/Services/UserServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,41 +13,23 @@ using Moq;
 
 namespace Mini.Wms.MongoDbImplementation.Tests.Services;
 
-
-[TestCategory("Integration")]
 [TestClass()]
 public class UserServiceTests
 {
-    private IConfigurationRoot? config = new ConfigurationBuilder()
-        .AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly())
-        .AddJsonFile("appsettings.json")
-        .Build();
 
-    private IMongoClient mongoClient;
-    private IMongoDatabase database;
-    private IMongoCollection<IUser<string>> userCollection;
-
-    private Mock<ILogger<UserService<string>>> mockLogger = new Mock<ILogger<UserService<string>>>();
-    private UserService<string> userService;
+    private Mock<ILogger<UserService>> mockLogger = new Mock<ILogger<UserService>>();
+    private Mock<IMongoCollection<User>> mockUserCollection = new Mock<IMongoCollection<User>>();
 
     [TestInitialize]
     public void BeforeEachTest()
     {
-        string miniToolsConnectionString = config["mongodb:minitools:ConnectionString"];
-        string databaseName = MongoUrl.Create(miniToolsConnectionString).DatabaseName;
 
-        mongoClient = new MongoClient(miniToolsConnectionString);
-        database = mongoClient.GetDatabase(databaseName);
-        userCollection = database.GetCollection<IUser<string>>(typeof(User).Name);
-
-        mockLogger = new Mock<ILogger<UserService<string>>>();
     }
 
-    
     [TestMethod()]
     public void UserServiceTest()
     {
-        userService = new UserService<string>(mockLogger.Object, userCollection);
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
 
         Assert.IsNotNull(userService);
     }
@@ -52,9 +37,8 @@ public class UserServiceTests
     [TestMethod()]
     public async Task AddAsyncTestAsync()
     {
-        userService = new UserService<string>(mockLogger.Object, userCollection);
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
 
-        //IUser<string> data = new 
         User record = new User()
         {
             Username = "someUsername",
@@ -62,48 +46,213 @@ public class UserServiceTests
             LastName = "someLastName"
         };
 
-        try
-        {
-            var result = await userService.AddAsync(record);
-        }
-        catch (System.Exception ex)
-        {
+        await userService.AddAsync(record);
 
-            throw;
-        }
-        
-
-        Assert.IsNotNull(userService);
-        Assert.Fail();
+        mockUserCollection.Verify(m => m.InsertOneAsync(It.IsAny<User>(), It.IsAny<InsertOneOptions>(), It.IsAny<CancellationToken>())
+        , Times.Once());
     }
 
-    //[TestMethod()]
-    //public void AllAsyncTest()
-    //{
-    //    Assert.Fail();
-    //}
+    [TestMethod()]
+    public async Task AllAsyncTestAsync()
+    {
+        Mock<IAsyncCursor<User>> mockCursor = new Mock<IAsyncCursor<User>>();
 
-    //[TestMethod()]
-    //public void DeleteAsyncTest()
-    //{
-    //    Assert.Fail();
-    //}
+        //mockCursor.Setup(m => m.ToListAsync(It.IsAny<CancellationToken>()))
+        //    .ReturnsAsync(new List<User>());
+        //Mock<IAsyncCursor<Book>> _bookCursor = new Mock<IAsyncCursor<Book>>();
+        mockCursor.Setup(m => m.Current).Returns(new List<User>());
+        //mockCursor
+        //    .SetupSequence(m => m.MoveNext(It.IsAny<CancellationToken>()))
+        //    .Returns(true)
+        //    .Returns(false);
+        mockCursor.SetupSequence(m => m.MoveNextAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(true))
+            .Returns(Task.FromResult(false));
 
-    //[TestMethod()]
-    //public void DeleteAsyncTest1()
-    //{
-    //    Assert.Fail();
-    //}
 
-    //[TestMethod()]
-    //public void GetAsyncTest()
-    //{
-    //    Assert.Fail();
-    //}
+        //mockUserCollection.Setup(m => m.Find)
+        mockUserCollection.Setup(m => m.FindAsync(It.IsAny<FilterDefinition<User>>(),
+            It.IsAny<FindOptions<User, User>>(),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockCursor.Object);
 
-    //[TestMethod()]
-    //public void UpdateAsyncTest()
-    //{
-    //    Assert.Fail();
-    //}
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
+
+        var result = await userService.AllAsync();
+
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod()]
+    public async Task DeleteAsyncTestAsync()
+    {
+        mockUserCollection.Setup(m => m.DeleteOneAsync(It.IsAny<FilterDefinition<User>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new DeleteResult.Acknowledged(1));
+
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
+
+        bool result = await userService.DeleteAsync("asd");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod()]
+    public async Task GetAsyncTestAsync()
+    {
+        Mock<IAsyncCursor<User>> mockCursor = new Mock<IAsyncCursor<User>>();
+
+        //mockCursor.Setup(m => m.FirstOrDefaultAsync(It.IsAny<CancellationToken>()))
+        //    .ReturnsAsync(new User());
+        mockCursor.Setup(m => m.Current).Returns(new List<User> { new User(), new User() });
+        //mockCursor
+        //    .SetupSequence(m => m.MoveNext(It.IsAny<CancellationToken>()))
+        //    .Returns(true)
+        //    .Returns(false);
+        mockCursor.SetupSequence(m => m.MoveNextAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(true))
+            .Returns(Task.FromResult(false));
+
+        //mockUserCollection.Setup(m => m.Find)
+        mockUserCollection.Setup(m => m.FindAsync(It.IsAny<FilterDefinition<User>>(),
+            It.IsAny<FindOptions<User, User>>(),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockCursor.Object);
+
+        //mockUserCollection.Setup(m => m.FindAsync(It.IsAny<FilterDefinition<User>>(), It.IsAny<FindOptions<User, User>>(), It.IsAny<CancellationToken>()))
+        //    .ReturnsAsync(new )
+
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
+
+        var result = await userService.GetAsync("asd");
+
+        Assert.IsNotNull(result);
+
+    }
+
+    [TestMethod()]
+    public async Task UpdateAsyncTestAsync()
+    {
+        mockUserCollection.Setup(m => m.FindOneAndUpdateAsync(It.IsAny<FilterDefinition<User>>(),
+            It.IsAny<UpdateDefinition<User>>(),
+            It.IsAny<FindOneAndUpdateOptions<User, User>>(),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new User
+            {
+            });
+
+        UserService userService = new UserService(mockLogger.Object, mockUserCollection.Object);
+
+        User record = new User()
+        {
+            Id="asd",
+            Username = "someUsername",
+            FirstName = "someFirstName",
+            LastName = "someLastName"
+        };
+
+        var result = await userService.UpdateAsync(record);
+
+        Assert.IsNotNull(result);
+    }
 }
+
+//namespace Mini.Wms.MongoDbImplementation.Tests.Services;
+
+
+//[TestClass()]
+//public class UserServiceTests
+//{
+//    private readonly IConfigurationRoot? config = new ConfigurationBuilder()
+//        .AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly())
+//        .AddJsonFile("appsettings.json")
+//        .Build();
+
+//    private Mock<ILogger<UserService>> mockLogger = new Mock<ILogger<UserService>>();
+
+//    private IMongoClient mongoClient;
+//    private IMongoDatabase database;
+//    private IMongoCollection<User> userCollection;
+//    private UserService? userService;
+
+//    public UserServiceTests()
+//    {
+//        string miniToolsConnectionString = config["mongodb:minitools:ConnectionString"];
+//        string databaseName = MongoUrl.Create(miniToolsConnectionString).DatabaseName;
+
+//        mongoClient = new MongoClient(miniToolsConnectionString);
+//        database = mongoClient.GetDatabase(databaseName);
+//        userCollection = database.GetCollection<User>(typeof(User).Name);
+//    }
+
+//    [TestInitialize]
+//    public void BeforeEachTest()
+//    {
+//        mockLogger = new Mock<ILogger<UserService>>();
+
+//        string miniToolsConnectionString = config["mongodb:minitools:ConnectionString"];
+
+//        string databaseName = MongoUrl.Create(miniToolsConnectionString).DatabaseName;
+
+//        mongoClient = new MongoClient(miniToolsConnectionString);
+//        database = mongoClient.GetDatabase(databaseName);
+//        userCollection = database.GetCollection<User>(typeof(User).Name);
+
+//    }
+
+
+//    [TestMethod()]
+//    public void UserServiceTest()
+//    {
+//        userService = new UserService(mockLogger.Object, userCollection);
+
+//        Assert.IsNotNull(userService);
+//    }
+
+//    [TestMethod()]
+//    public async Task AddAsyncTestAsync()
+//    {
+//        userService = new UserService(mockLogger.Object, userCollection);
+
+//        //IUser<string> data = new 
+//        User record = new User()
+//        {
+//            Username = "someUsername3",
+//            FirstName = "someFirstName3",
+//            LastName = "someLastName3"
+//        };
+
+//        await userService.AddAsync(record);
+
+//        Assert.IsNotNull(record.Id);
+//    }
+
+//    //[TestMethod()]
+//    //public void AllAsyncTest()
+//    //{
+//    //    Assert.Fail();
+//    //}
+
+//    //[TestMethod()]
+//    //public void DeleteAsyncTest()
+//    //{
+//    //    Assert.Fail();
+//    //}
+
+//    //[TestMethod()]
+//    //public void DeleteAsyncTest1()
+//    //{
+//    //    Assert.Fail();
+//    //}
+
+//    //[TestMethod()]
+//    //public void GetAsyncTest()
+//    //{
+//    //    Assert.Fail();
+//    //}
+
+//    //[TestMethod()]
+//    //public void UpdateAsyncTest()
+//    //{
+//    //    Assert.Fail();
+//    //}
+//}
