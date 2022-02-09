1a8269e7fc6507f977ba8f936aa551ec807a18c9
Zhixian Ong <zhixian@hotmail.com>
2022-02-09 14:16:16 +0800

    Add paged data objects

---
+++ Mini.Wms.DomainMessages/Models/PagedData.cs
@@ -0,0 +1,8 @@
+﻿namespace Mini.Wms.DomainMessages.Models;
+
+public readonly record struct PagedData<T>
+{
+    public long TotalRecordCount { get; init; }
+
+    public IEnumerable<T> Data { get; init; }
+}
